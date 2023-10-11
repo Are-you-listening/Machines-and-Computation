dde20f24fcb2fafd7ef1567cce000a2780b7141d
JeCheeseSmith <kars@duck.com>
2023-10-11 20:51:00 +0200

    Google Test Setup

--- test/test-main.cpp
+++ test/test-main.cpp
@@ -3,8 +3,20 @@
 //
 
 #include <iostream>
+#include <gtest/gtest.h>
 
-int test() {
-    std::cout << "We love Tibo" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+
+    std::cout << "main test" << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
 }
