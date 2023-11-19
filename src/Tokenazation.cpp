a9d7364d6808f185652ee7b7fd749d68f5da7fe7
AnasStitou <146850079+AnasStitou@users.noreply.github.com>
2023-11-19 14:29:16 +0100

    updated tokenazation, now finds when a Variable is used, not which, and Declaration is used, which it specfifes with D1.

--- src/Tokenazation.cpp
+++ src/Tokenazation.cpp
@@ -25,6 +25,43 @@ void Tokenazation::Tokenize(const std::string &FileLocation) {
             tokenVector.emplace_back("e",line);
         } else if(line.find("else")!=std::string::npos){
             tokenVector.emplace_back("E",line);
+        } else if(line.find(' ')!=std::string::npos&&(line.find('=')!=std::string::npos||((line.find('{')!=std::string::npos||line.find('(')!=std::string::npos)&&!(line.find('{')!=std::string::npos&&line.find('(')!=std::string::npos)))){ // this part finds declarations in code
+            for(auto it=line.cbegin(); it!=line.cend(); it++){
+                if(*it==' '){
+                    it++; 
+                    if(*it!='='){
+                        tokenVector.emplace_back("D",line);
+                        tokenVector.emplace_back("D1",line.substr(line.find(' ')+1,line.find('=')-(line.find(' ')+1))); // variable name;
+                        if(tokenVector[tokenVector.size()-1].second.find(" ")!=std::string::npos){
+                            tokenVector[tokenVector.size()-1].second=tokenVector[tokenVector.size()-1].second.substr(0,tokenVector[tokenVector.size()-1].second.size()-1);
+                        }
+                        break;
+                    }
+                }
+            }
+        } else if(line.find(' ')!=std::string::npos&&line.find('=')==std::string::npos&&line.find(';')!=std::string::npos){
+            unsigned int isDeclarationCount=0;
+            for(char it : line){
+                if(it==' '){
+                    isDeclarationCount++;
+                }
+            }
+            if(isDeclarationCount==1){
+                tokenVector.emplace_back("D",line);
+                tokenVector.emplace_back("D1",line.substr(line.find(' ')+1,line.size()-1));
+            }
+        } else if((line.find('+')!=std::string::npos||line.find('-')!=std::string::npos||line.find('*')!=std::string::npos // this just gives on when a variable is used, not which or even which scope the variable is from
+        ||line.find('/')!=std::string::npos||line.find('%')!=std::string::npos||line.find("++")!=std::string::npos // so use in combination of (D,line) to dether the scopes.
+        ||line.find("--")!=std::string::npos||line.find("++")!=std::string::npos||line.find('=')!=std::string::npos
+        ||line.find("+=")!=std::string::npos||line.find("-=")!=std::string::npos||line.find("*=")!=std::string::npos
+        ||line.find("/=")!=std::string::npos||line.find("%=")!=std::string::npos||line.find("==")!=std::string::npos
+        ||line.find("!=")!=std::string::npos||line.find('>')!=std::string::npos||line.find('<')!=std::string::npos
+        ||line.find(">=")!=std::string::npos||line.find("<=")!=std::string::npos||line.find("&&")!=std::string::npos
+        ||line.find("||")!=std::string::npos||line.find('!')!=std::string::npos||line.find('&')!=std::string::npos
+        ||line.find("<<")!=std::string::npos||line.find(">>")!=std::string::npos||line.find("->")!=std::string::npos
+        ||line.find('.')!=std::string::npos||(line.find('(')!=std::string::npos&&line.find(')')!=std::string::npos
+        &&line.find("()")==std::string::npos))&&line.find('#')==std::string::npos&&line.find("){")==std::string::npos){
+            tokenVector.emplace_back("V",line);
         }
         long int bracecount=0;
         for(char it: line){
