1ea23c645d85fb83aa38f492a9643fa226839ccc
JeCheeseSmith <kars@duck.com>
2023-11-19 21:25:55 +0100

    Base Setup CFG Constructor

---
+++ src/CFGConstructor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by watson on 11/19/23.
+//
+
+#include "CFGConstructor.h"
---
+++ src/CFGConstructor.h
@@ -0,0 +1,14 @@
+//
+// Created by watson on 11/19/23.
+//
+
+#ifndef TOG_CFGCONSTRUCTOR_H
+#define TOG_CFGCONSTRUCTOR_H
+
+//Symbols: {,},F,I,E,e, C, V, D, V1, D1, ... Vn, Dn
+class CFGConstructor {
+
+};
+
+
+#endif //TOG_CFGCONSTRUCTOR_H
