fe4d76523fdb20195599aab2b35e4b68c399e8c6
tibov <tibo.verreycken@telenet.be>
2023-11-27 14:17:02 +0100

    changes CFG

--- src/CFGConstructor.cpp
+++ src/CFGConstructor.cpp
@@ -7,16 +7,19 @@
 CFG* createCFG(){
     //Declare used variables
     vector<string> V{"S","B", "N"+to_string(Config::getConfig()->getMaxNesting()), "T"}; //S is StartSymbol, B off Bullshit in Between, Last variable Nx
-    vector<pair< string, vector<string>>> P{ {"S", {"N0"} },  {"B",{}} , {"N"+to_string(Config::getConfig()->getMaxNesting()), {"{", "N1", "}"} }, {"N"+to_string(Config::getConfig()->getMaxNesting()), {"{", "B", "}"} }}; //Already define the simplest rule, epsilon rule, rule back to start
+    vector<pair< string, vector<string>>> P{ {"S", {"N0"} },  {"B",{}} , {"N"+to_string(Config::getConfig()->getMaxNesting()), {"{", "N1", "}"} }}; //Already define the simplest rule, epsilon rule, rule back to start
 
     //Define V names
     for(unsigned int i = 0 ; i<Config::getConfig()->getMaxNesting(); i++){
         V.push_back("N"+to_string(i));
         P.push_back({"N"+to_string(i), {"N"+to_string(i), "N"+to_string(i)}});
         P.push_back( {"N"+to_string(i), {"{", "N"+to_string(i+1), "}"} } ); // Add Nx -> { Nx+1 } - Speedup
-        P.push_back( {"N"+to_string(i), {"{","B", "}"} } );
+        P.push_back( {"N"+to_string(i), {"B"} } );
+
     }
 
+    P.push_back( {"N"+to_string(Config::getConfig()->getMaxNesting()), {"N"+to_string(Config::getConfig()->getMaxNesting()), "N"+to_string(Config::getConfig()->getMaxNesting())} } );
+    P.push_back( {"N"+to_string(Config::getConfig()->getMaxNesting()), {"B"} } );
     //Create other Productions
     for(auto &k: T){ //"Generic Programming"
         if (k == "{" || k == "}"){
