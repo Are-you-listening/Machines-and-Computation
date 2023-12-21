e50b162c076b137597e788e1fd2243625296695a
JeCheeseSmith <kars@duck.com>
2023-12-21 19:23:38 +0100

    added testcase

--- demo/combination.cpp
+++ demo/combination.cpp
@@ -1,3 +1,21 @@
 //
 // Created by watson on 12/21/23.
 //
+
+class Object{
+private:
+    int temp;
+public:
+    int test(int tibo) const{
+        ++tibo;
+        for(int k = 0; k<8; k++){
+            if(k>3){
+                int s;
+                while(s < 69){
+                    tibo+=8;
+                    ++s;
+                }
+            }
+        }
+    }
+};
