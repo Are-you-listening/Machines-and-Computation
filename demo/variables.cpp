0cc363551117fcb7d75ab3a1ff335f9c988db142
JeCheeseSmith <kars@duck.com>
2023-12-21 18:32:31 +0100

    updated testcase

--- demo/variables.cpp
+++ demo/variables.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <iostream>
 
-int* getPiece(int r, int k){
+int* function2(int r, int k){
     return nullptr;
 }
 
@@ -15,7 +15,7 @@ std::vector<int*> function(const int &color)  {
 
     for(int r=0 ; r<8 ; r++){
         for(int k=0 ; k<8 ; k++){
-            int* piece = getPiece(r,k);
+            int* piece = function2(r,k);
             if(piece!= nullptr){
                 if(false){
                     options.push_back(piece);
